01637b47a9f70093f1893e42539e6e30bb0cf805
teresaogayar <teresaogayarcristoreyjaen@gmail.com>
2021-08-17 16:08:59 +0200

    componente navegacion creada con enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navegacion', component: NavegacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
