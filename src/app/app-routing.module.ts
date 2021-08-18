c9aa232a01a749822f0584d6b5a215a6046e575b
teresaogayar <teresaogayarcristoreyjaen@gmail.com>
2021-08-18 10:40:24 +0200

    Cambios en persona.component.html, cambios en navegacion y algo de css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavegacionComponent } from './navegacion/navegacion.component';
 import { PersonaComponent } from './persona/persona.component';
 
 const routes: Routes = [
-  { path: 'navegacion', component: NavegacionComponent},
+
   { path: 'persona', component: PersonaComponent}
 ];
 
