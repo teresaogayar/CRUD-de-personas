187e08704f2794d847b870b81ef78c6148278aa1
teresaogayar <teresaogayarcristoreyjaen@gmail.com>
2021-08-18 13:03:47 +0200

    formulario.component.html y formulario.component.scss realizado con su enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
 import { PersonaComponent } from './persona/persona.component';
 
 
 const routes: Routes = [
-  { path: 'lista_persona', component: PersonaComponent}
+  { path: 'lista_persona', component: PersonaComponent},
+  { path: '', component: FormularioComponent}
 ];
 
 @NgModule({
